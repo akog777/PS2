840ad73f87582a749fa14d2e8af5658e7af01505
Isaac Koga <akog.sac@gmail.com>
2025-08-22 12:59:49 +0000

    atualizando

--- aula3_classeAbstratas/ReservaPassagens/src/App.java
+++ aula3_classeAbstratas/ReservaPassagens/src/App.java
@@ -6,24 +6,24 @@ import model.Passageiro;
 
 public class App {
     public static void main(String[] args) {
-        Assento a1 = new AssentoEconomica("Economica", 50.0);
+        Assento a1 = new AssentoEconomica("Economica", 500.0);
         //System.out.println("Valor do Assento: "+ a1.calcularPrecoFinal());
 
-        Assento a2 = new AssentoExecutiva("Executiva", 50.0);
+        Assento a2 = new AssentoExecutiva("Executiva", 500.0);
         //System.out.println("Valor do Assento: "+ a2.calcularPrecoFinal()); 
 
-        Assento a3 = new AssentoPrimeiraClasse("Primeira Classe", 50.0);
+        Assento a3 = new AssentoPrimeiraClasse("Primeira Classe", 500.0);
         //System.out.println("Valor do Assento: "+ a3.calcularPrecoFinal()); 
 
         Passageiro p1 = new Passageiro("Ronaldinho");
         System.out.println("Comprando assento para a classe economica.");
         p1.setAssento(a1);
 
-        System.out.println("O assento do Passageiro ["+p1.getNome()+"] custa [R$"+p1.getAssento().calcularPrecoFinal()+"]");
+        System.out.println("O assento do Passageiro ["+p1.getNome()+"] custa [R$"+p1.getAssento().calcularPrecoFinal()+"]\n");
 
         System.out.println("Realizando o upgrade para a classe executiva.");
         p1.setAssento(a2);
-        System.out.println("O assento do Passageiro ["+p1.getNome()+"] custa [R$"+p1.getAssento().calcularPrecoFinal()+"]");
+        System.out.println("O assento do Passageiro ["+p1.getNome()+"] custa [R$"+p1.getAssento().calcularPrecoFinal()+"]\n");
 
         System.out.println("Realizando o upgrade para a primeira classe.");
         p1.setAssento(a3);
