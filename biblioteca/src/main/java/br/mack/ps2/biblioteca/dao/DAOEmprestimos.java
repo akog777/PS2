dabba8bd9a14cec2cf6b720abd9caebde5bb0db1
Isaac Koga <akog.sac@gmail.com>
2025-10-03 11:06:51 -0300

    Delete biblioteca directory

--- biblioteca/src/main/java/br/mack/ps2/biblioteca/dao/DAOEmprestimos.java
+++
@@ -1,6 +0,0 @@
-package br.mack.ps2.biblioteca;
-
-import org.springframework.data.repository.CrudRepository;
-import ps2.biblioteca.model.Emprestimos;
-
-public interface DAOEmprestimos extends CrudRepository<Emprestimo, Long> {}
--- biblioteca/src/main/java/br/mack/ps2/biblioteca/dao/DAOLivros.java
+++
@@ -1,6 +0,0 @@
-package br.mack.ps2.biblioteca;
-
-import org.springframework.data.repository.CrudRepository;
-import ps2.biblioteca.model.Livros;
-
-public interface DAOLivros extends CrudRepository<Livros, Long> {}
